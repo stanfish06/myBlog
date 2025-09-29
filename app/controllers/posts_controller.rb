89b0bb320d8c7ed6ad99f3b68216d094a63c5822
Stan <zhiyuanyucleveland@outlook.com>
2025-09-29 01:19:04 -0400

    update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_post, only: %i[ show edit update destroy ]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+    def post_params
+      params.expect(post: [ :title, :body ])
+    end
+end
